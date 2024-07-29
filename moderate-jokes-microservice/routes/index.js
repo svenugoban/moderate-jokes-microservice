5647f701e97b88b54541363948f3b7890e3447d2
svenugoban <asvenugoban19962015@gmail.com>
2024-07-29 23:51:56 +0530

    Implement authentication using basic credentials.

--- moderate-jokes-microservice/routes/index.js
+++ moderate-jokes-microservice/routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.use(bodyParser.json());
+
+const jokes = [];
+const credentials = { email: "admin@admin.com", password: "admin123" };
+
+app.post("/auth", (req, res) => {
+  const { email, password } = req.body;
+  if (email === credentials.email && password === credentials.password) {
+    res.json({ token: "authenticated" });
+  } else {
+    res.status(401).send("Unauthorized");
+  }
 });
 
-module.exports = router;
+app.get("/jokes", (req, res) => {
+  res.json(jokes);
+});
+
+app.post("/jokes", (req, res) => {
+  jokes.push(req.body);
+  res.send("Joke added");
+});
+
+app.put("/jokes/:id", (req, res) => {
+  const joke = jokes[req.params.id];
+  if (joke) {
+    Object.assign(joke, req.body);
+    res.send("Joke updated");
+  } else {
+    res.status(404).send("Joke not found");
+  }
+});
+
+app.delete("/jokes/:id", (req, res) => {
+  jokes.splice(req.params.id, 1);
+  res.send("Joke deleted");
+});
+
+app.listen(3000, () => {
+  console.log("Moderate Jokes service running on port 3000");
+});
